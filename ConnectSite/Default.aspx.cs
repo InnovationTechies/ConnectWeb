68833a5e39dd32c45d394de52e2749f610ada7ae
PAsenoguan <PAsenoguan@PASENOGUAN-PC>
2018-06-15 22:05:04 +0200

    TimerUpdate

--- ConnectSite/Default.aspx.cs
+++ ConnectSite/Default.aspx.cs
@@ -4,13 +4,23 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Globalization;
+using System.Mime;
+using System.Net;
 
 namespace ConnectSite
 {
     public partial class _Default : Page
     {
+        string timeInter, timeVar;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            try
+            {
+                var httpWebRequest = (HttpWebRequest).WebRequest("")
+            }
+            lblTimer.Text = 
 
         }
     }
